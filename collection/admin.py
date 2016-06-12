efcfeddfcf172113027ccbc083f43fa9f6f974c8
kevin <kbigelow5@gmail.com>
2016-06-11 23:07:17 -0600

    created first migration and model for businesses

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import models
+from collection.models import business
+
+# Automated slug creation
+class businessAdmin(admin.ModelAdmin):
+    model = business
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# Register model
+admin.site.register(business, businessAdmin)
+
