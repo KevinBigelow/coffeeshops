7170726f2985c48266f61b5bfb54aef278509c17
kevin <kbigelow5@gmail.com>
2016-06-11 23:36:39 -0600

    interacting with the database in the templates :)

--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
+from collection.models import business
 
 # Create your views here.
 def index(request):
-    # Defining the variable
-    number = 6
-    name = 'kevin bigelow'
+    businesses = business.objects.all()
     # Passing the variable to the view
     return render(request, 'index.html', {
-        'number': number,
-        'name': name,
+        'business': businesses,
     })
